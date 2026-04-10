02152eda391c63472bb924a1d5965df80c7a65ac
Mrittick Choudhury <mrittick.choudhury@veradigm.com>
2026-04-10 10:44:54 +0530

    Bug fixes

---
+++ dist/payerpath/browser/chunk-FIB35C72.js
@@ -0,0 +1,1 @@
+import"./chunk-2NFLSA4Y.js";var o=[{path:"patient-demographics",loadComponent:()=>import("./chunk-HM2K6K6N.js").then(t=>t.PatientDemographicsComponent)}];export{o as patientsRoutes};
